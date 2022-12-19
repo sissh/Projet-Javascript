8e860f7d85537369a58820f822692735e850d5b5
sissh <sissani.hani@gmail.com>
2022-12-19 15:06:15 +0100

    New solution to ex 3

--- hani_sissani_test_javascript_algo_ex2.js
+++ hani_sissani_test_javascript_algo_ex2.js
@@ -12,5 +12,12 @@ function isDistinct (array) {
     return true
 }
 
+document.body.appendChild(document.createElement("br"))
 document.body.appendChild(document.createTextNode("Exercice 2: "))
-document.body.appendChild(document.createTextNode(isDistinct(to_verify[0])))
+document.body.appendChild(document.createElement("br"))
+document.body.appendChild(document.createElement("br"))
+
+for (var i = 0; i < 9; i++) {
+    document.body.appendChild(document.createTextNode("Ligne " + i + " : " + isDistinct(to_verify[i])))
+    document.body.appendChild(document.createElement("br"))
+}
--- hani_sissani_test_javascript_algo_ex3.js
+++ hani_sissani_test_javascript_algo_ex3.js
@@ -1,9 +1,13 @@
+var faultyRows = []
+var faultyColumns = []
+var faultyRegions = []
 //F31
 function checkTableRows (table) {
     for (var i = 0; i < table.length; i++) {
         if (!isDistinct(table[i])) {
             document.body.appendChild(document.createTextNode("Ligne " + i + " incorrecte : " + table[i]))
             document.body.appendChild(document.createElement("br"))
+            faultyRows.push(table[i])
         }
     }
 }
@@ -16,38 +20,63 @@ function getColumn(table, n) {
 //F32
 function checkTableColumns (table) {
     for (var i = 0; i < table.length; i++) {
-        colonne = getColumn(table, i)
-        if (!isDistinct(colonne)) {
-            document.body.appendChild(document.createTextNode("Colonne " + i + " incorrecte : " + colonne))
+        column = getColumn(table, i)
+        if (!isDistinct(column)) {
+            document.body.appendChild(document.createTextNode("Colonne " + i + " incorrecte : " + column))
             document.body.appendChild(document.createElement("br"))
+            faultyColumns.push(column)
         }
     }
 }
 
-//Was part of a solution I tried.
-function instantiateRegionList(table) {
-    listeRegions = new Array(9)
-    for (var i = 0; i < table.length; i++) {
-        region = new Array(table.length/3)
-        for (var j=0; j<table.length/3; j++) {
-            region[i] = new Array(table.length/3)
+//Create regions with data from the table. Probably not the best solution for this problem as it involves multiple nested loops.
+function createRegions(table) {
+    var stepRow = 0
+    var stepColumn = 0
+    var regionIndex = 0
+    var listeRegions = []
+    for (var i = 0; i < 3; i++) {
+        stepColumn = 0
+        for (var j = 0; j < 3; j++) {
+            var region = new Array(9)
+            regionIndex = 0
+            for (var k = 0; k < 3; k++) {
+                for (var l = 0; l < 3; l++) {
+                    region[regionIndex] = table[k + stepRow][l + stepColumn]
+                    regionIndex++
+                }
+            }
+            listeRegions.push(region)
+            stepColumn += 3
         }
-        listeRegions[i] = region
+        //this check prevents the index from going farther that what the array can hold
+        if(stepRow < 6) {
+            stepRow += 3
+        }
     }
     return listeRegions
 }
 
-//F33 - Could not be done on time.
+//F33 
 function checkTableRegions (table) {
-    listeRegions = fillRegions(table, instantiateRegionList(table))
-
-    console.log(listeRegions)
+    listeRegions = createRegions(table)
+    for (var i = 0; i < 9; i++) {
+        if(!isDistinct(listeRegions[i])) {
+            document.body.appendChild(document.createTextNode("Région " + i + " incorrecte : " + listeRegions[i]))
+            document.body.appendChild(document.createElement("br"))
+            faultyRegions.push(listeRegions[i])
+        }
+    }
 }
 
 document.body.appendChild(document.createElement("br"))
-document.body.appendChild(document.createElement("br"))
 document.body.appendChild(document.createTextNode("Exercice 3"))
 document.body.appendChild(document.createElement("br"))
+document.body.appendChild(document.createElement("br"))
+
 checkTableRows(to_verify)
 checkTableColumns(to_verify)
 checkTableRegions(to_verify)
+console.log(faultyColumns)
+console.log(faultyRegions)
+console.log(faultyRows)
