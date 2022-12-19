74d4d8ddf0b907824f910e18da4fc58ff5a0fdef
sissh <sissani.hani@gmail.com>
2022-12-19 09:35:49 +0100

    js

---
+++ javascript_test_je1.js
@@ -0,0 +1,10 @@
+var array_number = new Array(9);
+array_number[0]= "4 2 7 3 5 1 9 8 6";
+array_number[1]= "9 8 3 7 6 2 5 1 4";
+array_number[2]= "1 5 6 8 9 4 7 2 3";
+array_number[3]= "2 3 9 1 8 5 4 6 7";
+array_number[4]= "7 4 1 6 3 9 2 5 8";
+array_number[5]= "5 6 8 2 4 7 1 3 9";
+array_number[6]= "8 7 2 9 1 3 6 4 5";
+array_number[7]= "3 9 5 4 2 6 8 7 1";
+array_number[8]= "6 1 4 5 7 8 3 9 6";
